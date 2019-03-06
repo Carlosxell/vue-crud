d034f986dd80aa72718cb718724556fc80645991
carlos augusto <carlos.silva@processmind.com.br>
2019-03-06 15:52:50 -0300

    criação de arquivos iniciais, criação de arquivo para servidor, criação de arquivo para API

--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-984228.okta.com/oauth2/default',
+  client_id: '0oac0b5d3V74EM8xh356',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -14,12 +22,23 @@ export default new Router({
       component: Home
     },
     {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-})
+});
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router;
